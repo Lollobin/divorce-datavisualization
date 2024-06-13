692c3313b672acbbdcda025da877233a9b941e06
LisaTaimi <e11905185@student.tuwien.ac.at>
2024-06-13 16:45:10 +0200

    on deselect reset stroke colour and width

--- static/scripts/stateManager.js
+++ static/scripts/stateManager.js
@@ -1,18 +1,21 @@
-
 const selectedState = document.getElementById("selectedState");
 
-function changeState(newState){
+function changeState(newState) {
     updateMonthlyChart(nestedData.get(newState));
     selectedState.innerHTML = newState;
 }
+
 selectedState.innerHTML = states[0];
 
-function deselectStates(){
+function deselectStates() {
+    d3.selectAll(".mapfield")
+        .style("stroke", "white")
+        .style("stroke-width", 1.5);
     updateMonthlyChart(nestedData.get(states[0]));
     selectedState.innerHTML = states[0];
 }
 
-function changeYear(newYear){
+function changeYear(newYear) {
     slider.value = newYear;
     changeMapYear(slider.value);
     selectedYear.innerHTML = slider.value;
